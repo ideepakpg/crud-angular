439dbd67b84ddba775c2f4aaa54f49f043db4516
Deepak P G <deepakpg121@gmail.com>
2024-08-07 22:33:15 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using crud_angular_api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud_angular_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee([FromBody] Employee model)
+        {
+            await _employeeRepository.AddEmployee(model);
+            return Ok();
+        }
+    }
+}
---
+++ Data/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace crud_angular_api.Data
+{
+    public class EmployeeRepository
+    {
+        private readonly AppDbContext _appDbcontext;
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            _appDbcontext = appDbContext;
+        }
+
+        public async Task AddEmployee(Employee employee)
+        {
+            await _appDbcontext.Set<Employee>().AddAsync(employee);
+            await _appDbcontext.SaveChangesAsync();
+        }
+    }
+}
