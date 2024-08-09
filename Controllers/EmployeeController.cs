862d95066ce87def350c7f422e621d5f78f09ad4
Deepak P G <deepakpg121@gmail.com>
2024-08-09 22:49:33 +0530

    Add async CRUD endpoints to EmployeeController

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
-﻿using crud_angular_api.Data;
+using crud_angular_api.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace crud_angular_api.Controllers
 {
@@ -8,18 +9,49 @@ namespace crud_angular_api.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        private readonly EmployeeRepository _employeeRepository;
 
-        public EmployeeController(EmployeeRepository employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
+    private readonly EmployeeRepository _employeeRepository;
 
-        [HttpPost]
-        public async Task<ActionResult> AddEmployee([FromBody] Employee model)
-        {
-            await _employeeRepository.AddEmployee(model);
-            return Ok();
-        }
+    public EmployeeController(EmployeeRepository employeeRepository)
+    {
+      _employeeRepository = employeeRepository;
     }
+
+    [HttpPost]
+    public async Task<ActionResult> AddEmployee([FromBody] Employee model)
+    {
+      await _employeeRepository.AddEmployeeAsync(model);
+      return Ok();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetEmployeeList()
+    {
+      var employeeList = await _employeeRepository.GetAllEmployeeAsync();
+      return Ok(employeeList);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetEmployeeById([FromRoute] int id)
+    {
+      var employee = await _employeeRepository.GetEmployeeByIdAsync(id);
+      return Ok(employee);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateEmployee([FromRoute] int id, [FromBody] Employee model)
+    {
+      await _employeeRepository.UpdateEmployeeAsync(id, model);
+      return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteEmployee([FromRoute] int id)
+    {
+      await _employeeRepository.DeleteEmployeeAsnyc(id);
+      return Ok();
+    }
+
+  }
+
 }
