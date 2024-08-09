be382e35cc87e3e99c038d478096bddbdf26a4b7
Deepak P G <deepakpg121@gmail.com>
2024-08-09 22:48:24 +0530

    Add async CRUD operations to EmployeeRepository

--- Data/EmployeeRepository.cs
+++ Data/EmployeeRepository.cs
@@ -1,17 +1,58 @@
-﻿namespace crud_angular_api.Data
-{
-    public class EmployeeRepository
-    {
-        private readonly AppDbContext _appDbcontext;
-        public EmployeeRepository(AppDbContext appDbContext)
-        {
-            _appDbcontext = appDbContext;
-        }
+using Microsoft.EntityFrameworkCore;
 
-        public async Task AddEmployee(Employee employee)
-        {
-            await _appDbcontext.Set<Employee>().AddAsync(employee);
-            await _appDbcontext.SaveChangesAsync();
-        }
+namespace crud_angular_api.Data
+{
+  public class EmployeeRepository
+  {
+
+    private readonly AppDbContext _appDbcontext;
+
+    public EmployeeRepository(AppDbContext appDbContext)
+    {
+      _appDbcontext = appDbContext;
     }
+
+    public async Task AddEmployeeAsync(Employee employee)
+    {
+      await _appDbcontext.Set<Employee>().AddAsync(employee);
+      await _appDbcontext.SaveChangesAsync();
+    }
+
+    public async Task<List<Employee>> GetAllEmployeeAsync()
+    {
+      return await _appDbcontext.Employees.ToListAsync();
+    }
+
+    public async Task<Employee> GetEmployeeByIdAsync(int id)
+    {
+      return await _appDbcontext.Employees.FindAsync(id);
+    }
+
+    public async Task UpdateEmployeeAsync(int id, Employee model)
+    {
+      var employeee = await _appDbcontext.Employees.FindAsync(id);
+      if (employeee == null)
+      {
+        throw new Exception("Employee not found");
+      }
+      employeee.Name = model.Name;
+      employeee.Phone = model.Phone;
+      employeee.Age = model.Age;
+      employeee.Salary = model.Salary;
+      await _appDbcontext.SaveChangesAsync();
+    }
+
+    public async Task DeleteEmployeeAsnyc(int id)
+    {
+      var employeee = await _appDbcontext.Employees.FindAsync(id);
+      if (employeee == null)
+      {
+        throw new Exception("Employee not found");
+      }
+      _appDbcontext.Employees.Remove(employeee);
+      await _appDbcontext.SaveChangesAsync();
+    }
+
+  }
+
 }
