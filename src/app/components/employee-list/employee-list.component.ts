4de69904d024e8955e5cc6e7b728da868f8126d8
Deepak P G <deepakpg121@gmail.com>
2024-08-11 16:26:53 +0530

    Enhance EmployeeListComponent with edit and delete functionalities

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -3,7 +3,8 @@ import { IEmployee } from '../../interfaces/employee';
 import { HttpService } from '../../http.service';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-employee-list',
@@ -13,20 +14,39 @@ import { RouterLink } from '@angular/router';
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
+  router=inject(Router)
   employeeList: IEmployee[] = [];
   httpService = inject(HttpService);
+  toaster=inject(ToastrService)
   displayedColumns: string[] = [
     'id',
     'name',
     'age',
     'phone',
     'email',
-    'salary'
+    'salary',
+    'action'
   ];
   ngOnInit() {
+    this.getEmployeeFromServer();
+  }
+
+  getEmployeeFromServer(){
     this.httpService.getAllEmployee().subscribe((result: IEmployee[]) => {
       this.employeeList = result;
       console.log(this.employeeList);
+    });
+  }
+
+  edit(id:number){
+    console.log(id);
+    this.router.navigateByUrl("/employee/"+id);
+  }
+  delete(id: number) {
+    this.httpService.deleteEmployee(id).subscribe(() => {
+      console.log('deleted');
+      this.toaster.success('Record deleted sucessfully');
+      this.getEmployeeFromServer();
     });
   }
 }
